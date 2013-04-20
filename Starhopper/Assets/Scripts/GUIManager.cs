75fb7b8e40b091112e60e7fe6154f9504f72fb4f
martinl <martinl@stunlockstudios.com>
2013-04-20 16:09:24 +0000

    stuffs

--- Starhopper/Assets/Scripts/GUIManager.cs
+++ Starhopper/Assets/Scripts/GUIManager.cs
@@ -4,24 +4,54 @@ using System.Collections;
 [ExecuteInEditMode]
 public class GUIManager : MonoBehaviour {
 
-	public Texture TestTexture;
-
-	private float UniverseSliderValue = 1.0f;
+	public Texture ControlPanelBackground;
+	
+	float _DistanceFilter = 100f;
+	float _ToggleHeigth;
+	bool _ToggleUp;
 	
 	void Start()
 	{
 	}
 		
-	void UpdateHeader ()
+	void Update ()
 	{
+		if( Input.GetKeyDown(KeyCode.Tab) )
+			_ToggleUp = !_ToggleUp;
 	}
 	
 	void OnGUI()
 	{
 		GUI.color = Color.grey;
-		GUI.DrawTexture(new Rect(0,0, 130, 200), TestTexture);
+		GUI.DrawTexture(new Rect(0,0 - _ToggleHeigth, 130, 200), ControlPanelBackground);
 		GUI.color = Color.white;
-		UniverseSliderValue = GUI.HorizontalSlider(new Rect(15, 25, 100, 25), UniverseSliderValue, 0, 10);
-		GUI.Label(new Rect(15, 5, 100, 25), "Scale Universe");
+		
+		//Universe Scale
+		Scaler.Scale = GUI.HorizontalSlider(new Rect(15, 25 - _ToggleHeigth, 100, 25), Scaler.Scale, 0, 10);
+		GUI.Label(new Rect(15, 5 - _ToggleHeigth, 100, 25), "Scale Universe");
+		
+		//Distance Filter
+		_DistanceFilter = GUI.HorizontalSlider(new Rect(15, 65 - _ToggleHeigth, 100, 25), _DistanceFilter, 0, 1000);
+		GUI.Label(new Rect(15, 45 - _ToggleHeigth, 100, 25), "Distance Filter");
+		
+		//Toggle GUI
+		if( GUI.Button(new Rect(15, 165 - _ToggleHeigth, 100, 25), "Toggle (Tab)" ) )
+		{
+			_ToggleUp = !_ToggleUp;
+		}
+		if( _ToggleUp )
+		{
+			if( _ToggleHeigth < 160 )
+				_ToggleHeigth += Time.deltaTime * 300f;
+			else
+				_ToggleHeigth = 160;
+		}
+		else
+		{
+			if( _ToggleHeigth > 0 )
+				_ToggleHeigth -= Time.deltaTime * 300f;
+			else
+				_ToggleHeigth = 0;
+		}
 	}
 }
