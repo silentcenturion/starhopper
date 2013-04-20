54f618eaaade7eb1981cd291a2baec209d87d4cd
martinl@stunlockstudios.com <martinl@stunlockstudios.com@da80e6f0-5eee-a167-4e8e-317f4631fc5e>
2013-04-20 16:42:43 +0000

    more gui

--- trunk/Starhopper/Assets/Scripts/GUIManager.cs
+++ trunk/Starhopper/Assets/Scripts/GUIManager.cs
@@ -7,8 +7,11 @@ public class GUIManager : MonoBehaviour {
 	public Texture ControlPanelBackground;
 	
 	float _DistanceFilter = 100f;
-	float _ToggleHeigth;
-	bool _ToggleUp;
+	float _ToggleControlPanelHeigth;
+	bool _ToggleControlPanelUp;
+	float _HeaderHeigth;
+	bool _HeaderUp = true;
+	string _FocusedStarName;
 	
 	void Start()
 	{
@@ -17,41 +20,74 @@ public class GUIManager : MonoBehaviour {
 	void Update ()
 	{
 		if( Input.GetKeyDown(KeyCode.Tab) )
-			_ToggleUp = !_ToggleUp;
+			_ToggleControlPanelUp = !_ToggleControlPanelUp;
 	}
 	
 	void OnGUI()
 	{
+		//Controller Background
 		GUI.color = Color.grey;
-		GUI.DrawTexture(new Rect(0,0 - _ToggleHeigth, 130, 200), ControlPanelBackground);
+		GUI.DrawTexture(new Rect(0,0 - _ToggleControlPanelHeigth, 130, 200), ControlPanelBackground);
 		GUI.color = Color.white;
 		
 		//Universe Scale
-		Scaler.Scale = GUI.HorizontalSlider(new Rect(15, 25 - _ToggleHeigth, 100, 25), Scaler.Scale, 0, 10);
-		GUI.Label(new Rect(15, 5 - _ToggleHeigth, 100, 25), "Scale Universe");
+		Scaler.Scale = GUI.HorizontalSlider(new Rect(15, 25 - _ToggleControlPanelHeigth, 100, 25), Scaler.Scale, 0, 10);
+		GUI.Label(new Rect(15, 5 - _ToggleControlPanelHeigth, 100, 25), "Scale Universe");
 		
 		//Distance Filter
-		_DistanceFilter = GUI.HorizontalSlider(new Rect(15, 65 - _ToggleHeigth, 100, 25), _DistanceFilter, 0, 1000);
-		GUI.Label(new Rect(15, 45 - _ToggleHeigth, 100, 25), "Distance Filter");
+		_DistanceFilter = GUI.HorizontalSlider(new Rect(15, 65 - _ToggleControlPanelHeigth, 100, 25), _DistanceFilter, 0, 1000);
+		GUI.Label(new Rect(15, 45 - _ToggleControlPanelHeigth, 100, 25), "Distance Filter");
 		
-		//Toggle GUI
-		if( GUI.Button(new Rect(15, 165 - _ToggleHeigth, 100, 25), "Toggle (Tab)" ) )
+		//Hide / Show Header
+		GUIStyle headerStyle = new GUIStyle();
+		headerStyle.fontSize = 50;
+		headerStyle.normal.textColor = Color.white;
+		GUI.Label(new Rect(Screen.width / 2 - headerStyle.CalcSize(new GUIContent(_FocusedStarName)).x / 2, 30 - _HeaderHeigth, 100, 25), _FocusedStarName , headerStyle);
+		if( _HeaderUp )
 		{
-			_ToggleUp = !_ToggleUp;
-		}
-		if( _ToggleUp )
-		{
-			if( _ToggleHeigth < 160 )
-				_ToggleHeigth += Time.deltaTime * 300f;
+			if( _HeaderHeigth < 160 )
+				_HeaderHeigth += Time.deltaTime * 300f;
 			else
-				_ToggleHeigth = 160;
+				_HeaderHeigth = 160;
 		}
 		else
 		{
-			if( _ToggleHeigth > 0 )
-				_ToggleHeigth -= Time.deltaTime * 300f;
+			if( _HeaderHeigth > 0 )
+				_HeaderHeigth -= Time.deltaTime * 300f;
 			else
-				_ToggleHeigth = 0;
+				_HeaderHeigth = 0;
+		}
+		
+		//Toggle GUI
+		if( GUI.Button(new Rect(15, 165 - _ToggleControlPanelHeigth, 100, 25), "Toggle (Tab)" ) )
+		{
+			_ToggleControlPanelUp = !_ToggleControlPanelUp;
+		}
+		if( _ToggleControlPanelUp )
+		{
+			if( _ToggleControlPanelHeigth < 160 )
+				_ToggleControlPanelHeigth += Time.deltaTime * 300f;
+			else
+				_ToggleControlPanelHeigth = 160;
+		}
+		else
+		{
+			if( _ToggleControlPanelHeigth > 0 )
+				_ToggleControlPanelHeigth -= Time.deltaTime * 300f;
+			else
+				_ToggleControlPanelHeigth = 0;
 		}
 	}
+	
+	public void SetStarFocus(string starInFocus)
+	{
+		if( starInFocus == string.Empty || starInFocus == "" )
+			_HeaderUp = true;
+		else
+		{
+			_HeaderUp = false;
+			_FocusedStarName = starInFocus;
+		}
+	}
+	
 }
