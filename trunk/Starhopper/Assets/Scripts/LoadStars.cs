d403b8ab2b1db7410600345deca72af5264696b0
philipb@stunlockstudios.com <philipb@stunlockstudios.com@da80e6f0-5eee-a167-4e8e-317f4631fc5e>
2013-04-20 10:50:05 +0000

    Added loading/parsing of "real" data.

---
+++ trunk/Starhopper/Assets/Scripts/LoadStars.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System;
+
+public static class LoadStars
+{
+    public static Star[] Load()
+    {
+        Star[] stars = new Star[120000];
+
+        StreamReader sr = new StreamReader(@"hygxyz.csv");
+
+        // The first line has captions about the data...
+        sr.ReadLine();
+
+        int currentStar = 0;
+        while (sr.EndOfStream == false)
+        {
+            string line = sr.ReadLine();
+            stars[currentStar] = ParseStar(line);
+
+            currentStar++;
+
+            if (currentStar > stars.Length)
+                break;
+        }
+
+        Array.Resize(ref stars, currentStar);
+        return stars;
+    }
+
+    private static Star ParseStar(string data)
+    {
+        int i = 0;
+        Star star = new Star();
+        star.StarID = ParseInt(data, ref i);
+        star.HIP = ParseInt(data, ref i);
+        star.HD = ParseInt(data, ref i);
+        star.HR = ParseInt(data, ref i);
+        star.Gliese = ParseString(data, ref i);
+        star.BayerFlamsteed = ParseString(data, ref i);
+        star.ProperName = ParseString(data, ref i);
+        star.RA = ParseFloat(data, ref i);
+        star.Dec = ParseFloat(data, ref i);
+        star.Distance = ParseFloat(data, ref i);
+        star.PMRA = ParseFloat(data, ref i);
+        star.PMDec = ParseFloat(data, ref i);
+        star.RV = ParseFloat(data, ref i);
+        star.Mag = ParseFloat(data, ref i);
+        star.AbsMag = ParseFloat(data, ref i);
+        star.Spectrum = ParseString(data, ref i);
+        star.ColorIndex = ParseFloat(data, ref i);
+        star.X = ParseFloat(data, ref i);
+        star.Y = ParseFloat(data, ref i);
+        star.Z = ParseFloat(data, ref i);
+        star.VX = ParseFloat(data, ref i);
+        star.VY = ParseFloat(data, ref i);
+        star.VZ = ParseFloat(data, ref i);
+
+        return star;
+    }
+
+    private static string ParseString(string data, ref int i)
+    {
+        string text = Parse(data, i);
+
+        if (text.Length == 0)
+        {
+            i++;
+            return string.Empty;
+        }
+
+        i += text.Length + 1;
+        return text;
+    }
+
+    private static float ParseFloat(string data, ref int i)
+    {
+        string text = Parse(data, i);
+
+        if (text.Length == 0)
+        {
+            i++;
+            return 0;
+        }
+
+        i += text.Length + 1;
+        return float.Parse(text);
+    }
+
+    private static int ParseInt(string data, ref int i)
+    {
+        string text = Parse(data, i);
+
+        if (text.Length == 0)
+        {
+            i++;
+            return 0;
+        }
+
+        i += text.Length + 1;
+        return int.Parse(text);
+    }
+
+    private static string Parse(string data, int i)
+    {
+        int index = data.IndexOf(',', i) - i;
+        string text;
+        if (index < 0)
+            text = data.Substring(i, data.Length - i);
+        else
+            text = data.Substring(i, index);
+        return text;
+    }
+}
---
+++ trunk/Starhopper/Assets/Scripts/Star.cs
@@ -0,0 +1,26 @@
+﻿public struct Star
+{
+    public int StarID;
+    public int HIP;
+    public int HD;
+    public int HR;
+    public string Gliese;
+    public string BayerFlamsteed;
+    public string ProperName;
+    public float RA;
+    public float Dec;
+    public float Distance;
+    public float PMRA;
+    public float PMDec;
+    public float RV;
+    public float Mag;
+    public float AbsMag;
+    public string Spectrum;
+    public float ColorIndex;
+    public float X;
+    public float Y;
+    public float Z;
+    public float VX;
+    public float VY;
+    public float VZ;
+}
