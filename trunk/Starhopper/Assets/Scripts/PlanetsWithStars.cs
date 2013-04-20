70e9c06d7ef37a6b17815a8efb187dce4846fb08
philipb@stunlockstudios.com <philipb@stunlockstudios.com@da80e6f0-5eee-a167-4e8e-317f4631fc5e>
2013-04-20 19:49:37 +0000

    Optimizations and camera speed fix.

--- trunk/Starhopper/Assets/Scripts/PlanetsWithStars.cs
+++ trunk/Starhopper/Assets/Scripts/PlanetsWithStars.cs
@@ -10,7 +10,7 @@ public class PlanetsWithStars : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        stars = LoadStars.Load();
+        stars = StarPicker.Stars;
 
         starExoplanets = LoadExoplanet.Load(stars);
         Debug.Log("Stars with planets = " + starExoplanets.Count);
@@ -27,16 +27,20 @@ public class PlanetsWithStars : MonoBehaviour
             Star star = stars[starID]; // todo: make sure that its the correct star...
 
             Vector3 starPos = new Vector3(star.X, star.Y, star.Z) * Scaler.Scale;
+            Vector3 camPos = Camera.mainCamera.transform.position;
+            float distance3D = Vector3.SqrMagnitude(camPos - starPos);
+            if (distance3D < 200 * 200 * Scaler.Scale)
+            {
+                Vector3 screenPos3D = Camera.mainCamera.WorldToScreenPoint(starPos);
+                screenPos3D.y = Screen.height - screenPos3D.y;
 
-            Vector3 screenPos3D = Camera.mainCamera.WorldToScreenPoint(starPos);
-            screenPos3D.y = Screen.height - screenPos3D.y;
+                if (screenPos3D.z < 0)
+                    continue;
 
-            if (screenPos3D.z < 0)
-                continue;
-
-            float distance = Vector3.Distance(starPos, Camera.main.transform.position);
-            Vector2 screenPos = new Vector2(screenPos3D.x, screenPos3D.y);
-            GUI.Label(new Rect(screenPos.x, screenPos.y, 200, 100), "Planets: " + planets.Count + " Dist: " + distance);
+                float distance = Vector3.Distance(starPos, Camera.main.transform.position);
+                Vector2 screenPos = new Vector2(screenPos3D.x, screenPos3D.y);
+                GUI.Label(new Rect(screenPos.x, screenPos.y, 200, 100), "Planets: " + planets.Count + " Dist: " + distance);
+            }
         }
     }
 }
