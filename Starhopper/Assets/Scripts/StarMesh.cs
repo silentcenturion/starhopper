a8e7872848193480b927c211220e6bc46da97c39
philipb <philipb@stunlockstudios.com>
2013-04-20 10:50:05 +0000

    Added loading/parsing of "real" data.

--- Starhopper/Assets/Scripts/StarMesh.cs
+++ Starhopper/Assets/Scripts/StarMesh.cs
@@ -7,33 +7,35 @@ public class StarMesh : MonoBehaviour
 {
     [ContextMenu("Generate Mesh")]
     void GenerateMesh()
-    {        
+    {
         int starCount = 14000;
 
-        Vector3[] stars = new Vector3[starCount];
+        Star[] stars = LoadStars.Load();
+        Vector3[] starVectors = new Vector3[starCount];
         for (int i = 0; i < starCount; i++)
         {
-            stars[i] = Random.onUnitSphere * Random.Range(-10000, 10000);
+            starVectors[i] = new Vector3(stars[i].X, stars[i].Y, stars[i].Z);
+            //starVectors[i] = Random.onUnitSphere * Random.Range(-10000, 10000);
         }
 
-        Vector3[] vertices = new Vector3[stars.Length * 4];
-        Vector2[] uvs = new Vector2[stars.Length * 4];
-        int[] triangles = new int[stars.Length * 6];
+        Vector3[] vertices = new Vector3[starVectors.Length * 4];
+        Vector2[] uvs = new Vector2[starVectors.Length * 4];
+        int[] triangles = new int[starVectors.Length * 6];
 
         int vertIndex = 0;
         int uvIndex = 0;
         int triangleIndex = 0;
-        for (int i = 0; i < stars.Length; i++)
+        for (int i = 0; i < starVectors.Length; i++)
         {
             int vert1 = vertIndex++;
             int vert2 = vertIndex++;
             int vert3 = vertIndex++;
             int vert4 = vertIndex++;
-            
-            vertices[vert1] = stars[i];
-            vertices[vert2] = stars[i];
-            vertices[vert3] = stars[i];
-            vertices[vert4] = stars[i];
+
+            vertices[vert1] = starVectors[i];
+            vertices[vert2] = starVectors[i];
+            vertices[vert3] = starVectors[i];
+            vertices[vert4] = starVectors[i];
 
             uvs[uvIndex++] = new Vector2(1, 1);
             uvs[uvIndex++] = new Vector2(1, -1);
@@ -54,7 +56,7 @@ public class StarMesh : MonoBehaviour
         mesh.uv = uvs;
         mesh.triangles = triangles;
         mesh.name = "starMesh";
-        
+
         MeshFilter meshFilter = GetComponent<MeshFilter>();
         meshFilter.sharedMesh = mesh;
     }
