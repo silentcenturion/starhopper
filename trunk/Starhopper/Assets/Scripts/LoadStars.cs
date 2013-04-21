f5df7f655867ada1836f5fad49c86e3706eafbce
karlb@stunlockstudios.com <karlb@stunlockstudios.com@da80e6f0-5eee-a167-4e8e-317f4631fc5e>
2013-04-21 07:15:24 +0000

    Asteroids!

--- trunk/Starhopper/Assets/Scripts/LoadStars.cs
+++ trunk/Starhopper/Assets/Scripts/LoadStars.cs
@@ -31,83 +31,31 @@ public static class LoadStars
     {
         int i = 0;
         Star star = new Star();
-        star.StarID = ParseInt(data, ref i);
-        star.HIP = ParseInt(data, ref i);
-        star.HD = ParseInt(data, ref i);
-        star.HR = ParseInt(data, ref i);
-        star.Gliese = ParseString(data, ref i);
-        star.BayerFlamsteed = ParseString(data, ref i);
-        star.ProperName = ParseString(data, ref i);
-        star.RA = ParseFloat(data, ref i);
-        star.Dec = ParseFloat(data, ref i);
-        star.Distance = ParseFloat(data, ref i);
-        star.PMRA = ParseFloat(data, ref i);
-        star.PMDec = ParseFloat(data, ref i);
-        star.RV = ParseFloat(data, ref i);
-        star.Mag = ParseFloat(data, ref i);
-        star.AbsMag = ParseFloat(data, ref i);
-        star.Spectrum = ParseString(data, ref i);
-        star.ColorIndex = ParseFloat(data, ref i);
-        star.X = ParseFloat(data, ref i);
-        star.Y = ParseFloat(data, ref i);
-        star.Z = ParseFloat(data, ref i);
-        star.VX = ParseFloat(data, ref i);
-        star.VY = ParseFloat(data, ref i);
-        star.VZ = ParseFloat(data, ref i);
+        star.StarID = CSVUtils.ParseInt(data, ref i);
+        star.HIP = CSVUtils.ParseInt(data, ref i);
+        star.HD = CSVUtils.ParseInt(data, ref i);
+        star.HR = CSVUtils.ParseInt(data, ref i);
+        star.Gliese = CSVUtils.ParseString(data, ref i);
+        star.BayerFlamsteed = CSVUtils.ParseString(data, ref i);
+        star.ProperName = CSVUtils.ParseString(data, ref i);
+        star.RA = CSVUtils.ParseFloat(data, ref i);
+        star.Dec = CSVUtils.ParseFloat(data, ref i);
+        star.Distance = CSVUtils.ParseFloat(data, ref i);
+        star.PMRA = CSVUtils.ParseFloat(data, ref i);
+        star.PMDec = CSVUtils.ParseFloat(data, ref i);
+        star.RV = CSVUtils.ParseFloat(data, ref i);
+        star.Mag = CSVUtils.ParseFloat(data, ref i);
+        star.AbsMag = CSVUtils.ParseFloat(data, ref i);
+        star.Spectrum = CSVUtils.ParseString(data, ref i);
+        star.ColorIndex = CSVUtils.ParseFloat(data, ref i);
+        star.X = CSVUtils.ParseFloat(data, ref i);
+        star.Y = CSVUtils.ParseFloat(data, ref i);
+		star.Z = CSVUtils.ParseFloat(data, ref i);
+		star.VX = CSVUtils.ParseFloat(data, ref i);
+		star.VY = CSVUtils.ParseFloat(data, ref i);
+		star.VZ = CSVUtils.ParseFloat(data, ref i);
 
         return star;
     }
 
-    private static string ParseString(string data, ref int i)
-    {
-        string text = Parse(data, i);
-
-        if (text.Length == 0)
-        {
-            i++;
-            return string.Empty;
-        }
-
-        i += text.Length + 1;
-        return text;
-    }
-
-    private static float ParseFloat(string data, ref int i)
-    {
-        string text = Parse(data, i);
-
-        if (text.Length == 0)
-        {
-            i++;
-            return 0;
-        }
-
-        i += text.Length + 1;
-        return float.Parse(text);
-    }
-
-    private static int ParseInt(string data, ref int i)
-    {
-        string text = Parse(data, i);
-
-        if (text.Length == 0)
-        {
-            i++;
-            return 0;
-        }
-
-        i += text.Length + 1;
-        return int.Parse(text);
-    }
-
-    private static string Parse(string data, int i)
-    {
-        int index = data.IndexOf(',', i) - i;
-        string text;
-        if (index < 0)
-            text = data.Substring(i, data.Length - i);
-        else
-            text = data.Substring(i, index);
-        return text;
-    }
 }
